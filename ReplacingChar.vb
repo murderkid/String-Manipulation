1b98a2e8a99a1d99102a2707ac66d10663dc437c
HamzaKhan-TCS <55450906+HamzaKhan-TCS@users.noreply.github.com>
2019-10-03 04:09:26 -0700

    Add files via upload

---
+++ ReplacingChar.vb
@@ -0,0 +1,36 @@
+﻿Module ReplacingChar
+
+    Sub Main()
+        Dim String1, NewString As String
+        Dim Char1, Char2, Char3 As String
+
+        String1 = ""
+        Char1 = ""
+        Char2 = ""
+        Char3 = ""
+        NewString = ""
+
+        Console.Write("Please enter the String you want to edit: ")
+        String1 = Console.ReadLine
+        Console.Write("Please enter the charecter you want to replace: ")
+        Char1 = Console.ReadLine
+        Console.Write("Please enter the charecter you want to replace With: ")
+        Char2 = Console.ReadLine
+
+        For Count = 1 To Len(String1)
+            Char3 = Mid(String1, Count, 1)
+            If Asc(Char3) = Asc(Char1) Then
+                NewString = NewString & Char2
+            Else
+                NewString = NewString & Char3
+
+            End If
+        Next
+        Console.Write(NewString)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
