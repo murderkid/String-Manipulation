2db6d8de03dedea1813e800fa8b76f66c3433c92
HamzaKhan-TCS <55450906+HamzaKhan-TCS@users.noreply.github.com>
2019-10-08 03:07:28 -0700

    Add files via upload

---
+++ ReverseString.vb
@@ -0,0 +1,27 @@
+﻿Module ReverseString
+
+    Sub Main()
+        Dim x As Integer
+        Dim Inputstring As String
+        Dim NextChar As String
+        Dim Outstring As String
+
+        Inputstring = ""
+        Outstring = ""
+        NextChar = ""
+        x = 0
+
+        Console.Write("Plz enter the string you want to reverse: ")
+        Inputstring = Console.ReadLine
+
+        For x = Len(Inputstring) To 1 Step -1
+            NextChar = Mid(Inputstring, x, 1)
+            Outstring = Outstring & NextChar
+        Next
+        Console.WriteLine(Outstring)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
