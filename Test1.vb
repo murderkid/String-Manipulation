a8967a8b113598f695588d82b6ca779c045b98b8
HamzaKhan-TCS <55450906+HamzaKhan-TCS@users.noreply.github.com>
2019-10-24 22:28:52 -0700

    Add files via upload

---
+++ Test1.vb
@@ -0,0 +1,42 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim BinStr, temp, Bit As String
+        Dim dec As Integer
+        Dim Length As Integer
+        Dim IsValid As Boolean = True
+
+        Console.WriteLine("Enter the Binary Number")
+        BinStr = Console.ReadLine
+
+        Length = Len(BinStr)
+
+        If Length > 8 Or Length < 1 Then
+
+            Console.WriteLine("Not Valid Binary Number")
+            IsValid = False
+        Else
+            For x = 1 To Length
+                Bit = Mid(BinStr, x, 1)
+                If Bit <> "0" And Bit <> "1" Then
+                    IsValid = False
+                    Console.WriteLine("Not Valid Binary Number")
+                End If
+
+                temp = Val(Mid(BinStr, Length, 1))
+                Length = Length - 1
+                If temp <> "0" Then
+                    dec += (2 ^ (x - 1))
+                End If
+            Next
+            If IsValid = True Then
+                Console.WriteLine("The Denary number of " & BinStr & " is : " & dec)
+            End If
+
+            Console.ReadKey()
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
